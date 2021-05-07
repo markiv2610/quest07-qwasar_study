0d68cbaf8d69eaca1338ec8a14f5130857087d4c
oksana markiv <markiv_o@qwasar.io>
2021-05-07 20:36:49 +0000

    My Array Uniq

---
+++ ex02/my_array_uniq.js
@@ -0,0 +1,6 @@
+function my_array_uniq(param_1) {
+    let chars = [param_1];
+	let uniqueChars = [...new Set(param_1)];
+ return uniqueChars;
+	console.log([1, 1, 2]); console.log([]); console.log([1, 1, 1, 2, 3, 4, 1]);
+	};
